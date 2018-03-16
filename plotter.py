146361180f1c6377861fdec88a9b2563963fe1d2
Davide <davidebelli95@gmail.com>
2018-03-16 15:20:06 +0100

    Created external function plotter.py

---
+++ plotter.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import math
+
+
+# plot losses on a unique figure 'plot.png'
+def plotter(D_G_zs, D_xs, Advs, L2s, G_tots, D_tots, points_per_epoch):
+    x = list(range(len(D_tots)))
+    log_4 = [-math.log(4)] * len(D_tots)
+    D_gain = [-k for k in D_tots]  # Discriminator gain defined as negative cross-entropy
+    
+    vline_position = [points_per_epoch * (x + 1) for x in range(int(math.floor(len(D_tots) / points_per_epoch)))]
+    plt.clf()
+    plt.plot(x, D_G_zs, "g-", linewidth=0.5, label="p D(G(z))")
+    plt.plot(x, D_xs, "r-", linewidth=0.5, label="p D(x)")
+    plt.plot(x, D_gain, "b-", linewidth=0.5, label="Disciminator")
+    plt.plot(x, log_4, "k--", linewidth=0.5, label="-log(4)")
+    plt.xlabel('x200 iterations')
+    plt.ylabel('value')
+    for k in vline_position:
+        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig("plots/main4.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
+    
+    plt.clf()
+    plt.plot(x, D_G_zs, "g-", linewidth=0.5, label="p D(G(z))")
+    plt.plot(x, D_xs, "r-", linewidth=0.5, label="p D(x)")
+    plt.xlabel('x200 iterations')
+    plt.ylabel('loss')
+    for k in vline_position:
+        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig("plots/fake-real_probs.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
+    
+    plt.clf()
+    plt.plot(x, Advs, "b-", linewidth=0.5, label="Adversarial loss")
+    plt.plot(x, L2s, "g-", linewidth=0.5, label="L2 loss")
+    plt.plot(x, G_tots, "k-", linewidth=0.5, label="Tot Generator loss")
+    plt.xlabel('x200 iterations')
+    plt.ylabel('loss')
+    for k in vline_position:
+        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig("plots/gen_losses.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
+    
+    plt.clf()
+    plt.plot(x, D_tots, "b-", linewidth=0.5, label="Tot Discriminator loss")
+    plt.xlabel('x200 iterations')
+    plt.ylabel('loss')
+    for k in vline_position:
+        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig("plots/disc_losses.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
+    
+    return
--- train.py
+++ train.py
@@ -13,11 +13,11 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
 import pickle
-import matplotlib.pyplot as plt
 import math
 import time
 
 from model import _netlocalD, _netG
+from plotter import plotter
 import utils
 
 parser = argparse.ArgumentParser()
@@ -52,12 +52,12 @@ opt = parser.parse_args()
 opt.cuda = True
 
 # opt.wtl2 = 0
-opt.ndf = 128 #Discriminator
-opt.nef = 128 #Generator
+# opt.ndf = 128 #Discriminator
+# opt.nef = 128 #Generator
 # opt.imageSize = 256
 # opt.patchSize = 128
 
-CONTINUE_TRAINING = True
+CONTINUE_TRAINING = False
 
 if CONTINUE_TRAINING:
     print("Continuing with the training of an existing model")
@@ -148,58 +148,6 @@ wtl2 = float(opt.wtl2)
 overlapL2Weight = 10
 
 
-# plot losses on a unique figure 'plot.png'
-def plotter(D_G_zs, D_xs, Advs, L2s, G_tots, D_tots):
-    x = list(range(len(D_tots)))
-    log_4 = [-math.log(4)] * len(D_tots)
-    D_gain = [-k for k in D_tots]  # Discriminator gain defined as negative cross-entropy
-    
-    vline_position = [len(dataloader)/STEPS_TO_PLOT * (x + 1) for x in range(int(math.floor(len(D_tots) * STEPS_TO_PLOT / len(dataloader))))]
-    plt.clf()
-    plt.plot(x, D_G_zs, "g-", linewidth=0.5, label="p D(G(z))")
-    plt.plot(x, D_xs, "r-", linewidth=0.5, label="p D(x)")
-    plt.plot(x, D_gain, "b-", linewidth=0.5, label="Disciminator")
-    plt.plot(x, log_4, "k--", linewidth=0.5, label="-log(4)")
-    plt.xlabel('x200 iterations')
-    plt.ylabel('value')
-    for k in vline_position:
-        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.savefig("plots/main4.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
-    
-    plt.clf()
-    plt.plot(x, D_G_zs, "g-", linewidth=0.5, label="p D(G(z))")
-    plt.plot(x, D_xs, "r-", linewidth=0.5, label="p D(x)")
-    plt.xlabel('x200 iterations')
-    plt.ylabel('loss')
-    for k in vline_position:
-        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.savefig("plots/fake-real_probs.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
-    
-    plt.clf()
-    plt.plot(x, Advs, "b-", linewidth=0.5, label="Adversarial loss")
-    plt.plot(x, L2s, "g-", linewidth=0.5, label="L2 loss")
-    plt.plot(x, G_tots, "k-", linewidth=0.5, label="Tot Generator loss")
-    plt.xlabel('x200 iterations')
-    plt.ylabel('loss')
-    for k in vline_position:
-        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.savefig("plots/gen_losses.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
-    
-    plt.clf()
-    plt.plot(x, D_tots, "b-", linewidth=0.5, label="Tot Discriminator loss")
-    plt.xlabel('x200 iterations')
-    plt.ylabel('loss')
-    for k in vline_position:
-        plt.axvline(x=k, linewidth=0.2, color='k', linestyle='--')
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.savefig("plots/disc_losses.png", bbox_extra_artists=(lgd,), bbox_inches='tight')
-    
-    return
-
-
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
@@ -261,7 +209,7 @@ optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
 # store information about losses for plotting
-STEPS_TO_PLOT = 200 # how often to update plots
+STEPS_TO_PLOT = 5 # how often to update plots
 N_UPDATE_GEN = 1 # update generator every N discriminator updates
 step_counter = 0
 
@@ -400,7 +348,7 @@ for epoch in range(resume_epoch, opt.niter):
             G_tots.append(this_G_tot)
             D_tots.append(this_D_tot)
             
-            plotter(D_G_zs, D_xs, Advs, L2s, G_tots, D_tots)
+            plotter(D_G_zs, D_xs, Advs, L2s, G_tots, D_tots, (len(dataloader)/STEPS_TO_PLOT))
 
             this_DGz = 0
             this_Dx = 0
