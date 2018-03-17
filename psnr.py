8064a510ef45b42b6123e2a4a3d01cc7e0943348
Davide <davidebelli95@gmail.com>
2018-03-17 19:36:16 +0100

    PSNR uses effective MAX_PIXEL intensity value

--- psnr.py
+++ psnr.py
@@ -6,8 +6,8 @@ def psnr(img1, img2):
     mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
-    PIXEL_MAX = 255
-    # PIXEL_MAX = max(np.amax(img1), np.amax(img2))
+    # PIXEL_MAX = 255
+    PIXEL_MAX = max(np.amax(img1), np.amax(img2))
     psnr_value = 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
     return psnr_value
 
